8109b9eff6afec4985a244460fb38081a2d68dfe
JesFot <jesfot@gmail.com>
2017-12-06 15:48:02 +0100

    Create error.php

---
+++ src/error.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<?php
+
+include_once('config.php');
+
+?>
+<html>
+<head>
+	<title>
+		<?php echo($ini_file["app_name"]); ?>
+	</title>
+	<meta charset="UTF-8" />
+	<link type="text/css" rel="stylesheet" href="style/main.css" />
+	<link type="text/css" rel="stylesheet" href="style/button.css" />
+	<link type="text/css" rel="stylesheet" href="style/icomoon.css" />
+</head>
+<body>
+	<h1>We are sorry</h1>
+	<p>An error occured during the process...<br />You can contact me at <a href="mailto:*******@********.fr">*******@********.fr</a></p>
+	<a href="./">Home</a>
+	<footer>
+		<h3>Hosted by <a href="http://fermedufariol.fr">La ferme du Fariol</a>, thanks to Denis Sicard.</h3>
+	</footer>
+</body>
+</html>
