79d2c8fcc5cf9ca201021ab59809e7c428b60de0
JesFot <jesfot@gmail.com>
2017-12-06 15:42:46 +0100

    Create badvalue.php

---
+++ src/badvalue.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<?php
+
+include_once('config.php');
+
+?>
+<html>
+<head>
+	<title>
+		<?php echo($ini_file["app_name"]); ?>
+	</title>
+	<meta charset="UTF-8" />
+	<link type="text/css" rel="stylesheet" href="style/main.css" />
+	<link type="text/css" rel="stylesheet" href="style/button.css" />
+	<link type="text/css" rel="stylesheet" href="style/icomoon.css" />
+</head>
+<body>
+	<h1>Bad value</h1>
+	<a href="./">Home</a>
+	<footer>
+		<h3>Hosted by <a href="http://fermedufariol.fr">La ferme du Fariol</a>, thanks to Denis Sicard.</h3>
+	</footer>
+</body>
+</html>
