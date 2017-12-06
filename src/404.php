10ab32b18c0927c0aee9d9c636b4afb910210053
JesFot <jesfot@gmail.com>
2017-12-06 15:39:15 +0100

    Create 404.php

---
+++ src/404.php
@@ -0,0 +1,13 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Error 404 - Page not found</title>
+</head>
+<body>
+	<h1>Erreur 404 - Page impossible à charger</h1>
+</body>
+</html>
